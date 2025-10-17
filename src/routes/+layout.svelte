7f83c4c2caa9e184069e069b62e854f44ae768c8
Dylan Adal <dylanadal.00@gmail.com>
2025-10-17 12:49:09 -0400

    works, trimming now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import '../app.css'
 	import faviconSVG from '$lib/assets/favicon.svg'
-	import favicon512 from '$lib/assets/favicon-512.png'
 	import webManifest from '$lib/assets/site.webmanifest'
 
 	import faviconApple from '$lib/assets/apple-touch-icon.png'
@@ -16,7 +15,7 @@
 	<title>Dylan Adal's Portfolio</title>
 
 	<!-- Favicon -->
-	<link rel="icon" type="image/png" href={favicon512}/>
+	<link rel="icon" type="image/png" href={faviconSVG}/>
 
 	<!-- Can't seem to get the icon working on safari dev -->
 	<link rel="apple-touch-icon" sizes="180x180" href={faviconApple} />
