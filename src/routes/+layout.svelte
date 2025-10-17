7f6388564b5b6e3944005ba0d1e831e1441c52a4
Dylan Adal <dylanadal.00@gmail.com>
2025-10-17 12:41:01 -0400

    try ico, move to static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '../app.css'
 	import faviconSVG from '$lib/assets/favicon.svg'
+	import favicon512 from '$lib/assets/favicon-512.png'
 	import webManifest from '$lib/assets/site.webmanifest'
+
+	import faviconApple from '$lib/assets/apple-touch-icon.png'
 
 	let { children } = $props()
 </script>
@@ -12,11 +15,11 @@
 
 	<title>Dylan Adal's Portfolio</title>
 
-	<!-- Standard favicon -->
-	<link rel="icon" type="image/svg+xml" href={faviconSVG}/>
+	<!-- Favicon -->
+	<link rel="icon" type="image/png" href={faviconSVG}/>
 
 	<!-- Can't seem to get the icon working on safari dev -->
-	<link rel="apple-touch-icon" sizes="180x180" href="%sveltekit.assets%/apple-touch-precomposed-icon.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href={faviconApple} />
 </svelte:head>
 
 {@render children?.()}
