f4989abd2af90f64ab8b035a34ac7c1d88eb7d5c
Dylan Adal <dylanadal.00@gmail.com>
2025-10-17 12:13:57 -0400

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
 	import '../app.css'
-	import favicon from '$lib/assets/favicon.svg'
+	import faviconSVG from '$lib/assets/favicon.svg'
+	import faviconApple from '$lib/assets/favicon-apple.png'
+	import webManifest from '$lib/assets/site.webmanifest'
 
 	let { children } = $props()
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<!-- Used for android devices pinning websites -->
+	<link rel="manifest" href={webManifest} />
+
+	<title>Dylan Adal's Portfolio</title>
+
+	<!-- Standard favicon -->
+	<link rel="icon" type="image/svg+xml" href={faviconSVG}/>
+
+	<!-- Can't seem to get the icon working on safari dev -->
+	<link rel="apple-touch-icon" href={faviconApple} />
 </svelte:head>
 
 {@render children?.()}
