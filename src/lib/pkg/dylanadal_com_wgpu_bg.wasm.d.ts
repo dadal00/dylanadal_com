77099fc06dbc7dfaf2b474d42648fb79c1ad5d50
dadal00 <148314527+dadal00@users.noreply.github.com>
2025-10-22 17:09:06 -0400

    floor of white blocks + independent lighting (#3)

---
+++ src/lib/pkg/dylanadal_com_wgpu_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run_web: () => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure979_externref_shim: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h96de1f5d11f53ff6: (a: number, b: number) => void;
+export const closure988_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure1677_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
